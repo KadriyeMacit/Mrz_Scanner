b89487870db584b485836b22105cf52b2023d734
Kadriye <kadriyemacit9655@gmail.com>
2020-04-07 17:48:37 +0300

    mrz_scanner

---
+++ deneme_mrz/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:deneme_mrz/camera_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FutureBuilder<Map<PermissionGroup, PermissionStatus>>(
+        future:
+        PermissionHandler().requestPermissions([PermissionGroup.camera]),
+        builder: (context, snapshot) {
+          if (snapshot.hasData &&
+              snapshot.data[PermissionGroup.camera] ==
+                  PermissionStatus.granted) {
+            return CameraPage();
+          }
+          return Scaffold(
+            body: Center(
+              child: Column(
+                children: const [
+                  CircularProgressIndicator(),
+                  Text('Awaiting for permissions')
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
